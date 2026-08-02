7d2dbe325fbdd14ef628e3d115a58bc89a654653
Matheus Mendes <matheusmendes.gestaopessoal@gmail.com>
2026-08-02 16:26:39 -0300

    Estrutura inicial do projeto

---
+++ codigo/principal.py
@@ -0,0 +1,10 @@
+from configuracoes.configuracao import NOME_EMPRESA
+
+def iniciar_projeto():
+    print("--------------------------------")
+    print(f"Simulador iniciado: {NOME_EMPRESA}")
+    print("--------------------------------")
+
+if __name__ == "__main__":
+    iniciar_projeto()
+
